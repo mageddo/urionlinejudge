e30f650f148eb75a74c06d3cf68f5fd5c6092d71
Elvis de Freitas <edigitalb@gmail.com>
2016-11-13 19:56:43 -0200

    fixando

--- src/main/c/iniciante/1160.c
+++ src/main/c/iniciante/1160.c
@@ -9,16 +9,14 @@ int main(){
 		scanf("%d %d %lf %lf", &aQtd, &bQtd, &aPer, &bPer);
 		aPer /= 100.0;
 		bPer /= 100.0;
-		aSum = (double)aQtd;
-		bSum = (double)bQtd;
+		aSum = aQtd;
+		bSum = bQtd;
 
 		for(i=1; i <= 100; i++){
 			if( (aSum += aSum * aPer) > (bSum += bSum * bPer) ){
-//				printf("%d %d\n", aSum, bSum);
 				printf("%d anos.\n", i);
 				break;
 			}
-//			printf("%d %d\n", aSum, bSum);
 		}
 		if(i == 101)
 			printf("Mais de 1 seculo.\n");

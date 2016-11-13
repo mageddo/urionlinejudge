adea148b1a59161b816ea4330f0a6e2e05ca9eb1
Elvis de Freitas <edigitalb@gmail.com>
2016-11-12 23:34:29 -0200

    criando testes unitarios

--- src/main/java/iniciante/P1046/MainTest.java
+++ src/main/java/iniciante/P1046/MainTest.java
@@ -1,13 +1,46 @@
 package iniciante.P1046;
 
-import static org.junit.Assert.*;
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * Created by elvis on 12/11/16.
  */
 public class MainTest {
-	@org.junit.Test
-	public void main() throws Exception {
+
+	@Test
+	public void main1() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		System.setIn(IOUtils.toInputStream("16 2\n"));
+		System.setOut(new PrintStream(out));
+		new Main().main(new String[]{});
+		Assert.assertEquals("O JOGO DUROU 10 HORA(S)\n", new String(out.toByteArray()));
+
+	}
+
+	@Test
+	public void main2() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		System.setIn(IOUtils.toInputStream("0 0\n"));
+		System.setOut(new PrintStream(out));
+		new Main().main(new String[]{});
+		Assert.assertEquals("O JOGO DUROU 24 HORA(S)\n", new String(out.toByteArray()));
+
+	}
+	@Test
+	public void main3() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		System.setIn(IOUtils.toInputStream("2 16\n"));
+		System.setOut(new PrintStream(out));
+		new Main().main(new String[]{});
+		Assert.assertEquals("O JOGO DUROU 14 HORA(S)\n", new String(out.toByteArray()));
 
 	}
 

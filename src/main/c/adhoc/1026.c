76313bdf34e22f672543a0d1c19275b9790149c2
Elvis de Freitas <edigitalb@gmail.com>
2016-11-14 12:04:51 -0200

    1026 nova solucao

--- src/main/c/adhoc/1026.c
+++ src/main/c/adhoc/1026.c
@@ -4,13 +4,18 @@
 
 int main(){
 
+	char *buf = malloc(sizeof(char) * BUFSIZ);
+	setbuf(stdout, buf);
+	setvbuf(stdout, buf, _IOFBF, BUFSIZ);
+
 	int a,b, len = 1 + sizeof(int);
-	char *str;
-	str = malloc(len);
+//	char *str;
+//	str = malloc(len);
 
 	for(; scanf("%u %u", &a, &b) != EOF; ){
-		sprintf(str, "%u\n", a ^ b);
-		write(STDOUT_FILENO, str, len);
+		printf("%u\n", a ^ b);
+//		sprintf(str, "%u\n", a ^ b);
+//		write(STDOUT_FILENO, str, len);
 	}
 	fflush(stdout);
 
